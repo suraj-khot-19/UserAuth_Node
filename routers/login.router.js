72e85729948181a3013c26e9013661741891c7c7
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-06-20 22:02:28 +0530

    test : Testing api end point and fixed bug(login.router.js)

--- routers/login.router.js
+++ routers/login.router.js
@@ -8,15 +8,17 @@ router.post("/login", async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        if (email.length < 0 || password.length < 0) {
+        if (email == null || password == null) {
             return res.status(400).json({
                 msg: "All feilds are required!"
             })
         }
 
+        // const user = await User.findOne({ email }).select('-password')   ///This wont work cause we need password below to check
+        
         const user = await User.findOne({ email })
 
-        if (user != null) {
+        if (user == null) {
             return res.status(400).json({
                 msg: "Email or Password combination is incorrect!"
             })
@@ -30,10 +32,15 @@ router.post("/login", async (req, res) => {
             })
         }
 
+        ///sending back only issential data except password 
+        const userObj = user.toObject();
+        delete userObj.password;
+
         res.status(200).json({
-            msg:"Logged in successfully!",
-            user:user
+            msg: "Logged in successfully!",
+            user: userObj
         })
+
     } catch (error) {
         res.status(500).json({
             msg: error.message

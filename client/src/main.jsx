6e7e0fbc11dd6d8c15b133fa66a67d5fbbd94e21
Anirudha Bele <beleanirudha8@gmail.com>
2025-06-24 13:37:48 +0530

    Feat: Create AuthContext to manage global user state and fetch current user on app load

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import router from "./router.jsx";
 import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<AuthProvider>
+			<RouterProvider router={router} />
+		</AuthProvider>
 	</StrictMode>
 );

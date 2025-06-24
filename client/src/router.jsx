365bb89e38e0f3696394357693ab3f5e970b665c
Anirudha Bele <beleanirudha8@gmail.com>
2025-06-24 12:49:59 +0530

    Refactor: Integrate MainLayout from layouts folder into router with Signup route

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
-import Signup from "./components/Signup";
+import MainLayout from "./layouts/MainLayout";
+import Signup from "./pages/Signup";
 
 const router = createBrowserRouter([
 	{
-		path: "/signup",
-		element: <Signup />,
+		path: "/",
+		element: <MainLayout />,
+		children: [
+			{
+				path: "signup",
+				element: <Signup />,
+			},
+		],
 	},
 ]);
 

21864f05e0f81406d41f7cb8197b6659db6521d5
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-07-01 22:08:23 +0530

    feat : updated dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,7 +7,13 @@ const Dashboard = () => {
 		<div className="p-6">
 			<h2 className="text-2xl font-semibold mb-4">Welcome to your Dashboard</h2>
 			<p className="text-gray-700">
-				Logged in as: <strong>{user?.name}</strong> ({user?.email})
+				Logged in as: <strong>{user?.name}</strong>
+			</p>
+			<p>
+				Age : <strong>{user?.age}</strong>
+			</p>
+			<p>
+				Email : <strong>{user?.email}</strong>
 			</p>
 		</div>
 	);
